e9fa667f458df800c466200d6470db2cbb54ab52
Kio <kkudumu@gmail.com>
2017-11-06 18:39:43 -0800

    Initial Commit

---
+++ North_South_East_West/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  North_South_East_West
+//
+//  Created by Kioja Kudumu on 11/6/17.
+//  Copyright © 2017 Kioja Kudumu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
